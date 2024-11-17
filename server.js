94e91ef99864ab44cce2d21641b1f048f316f175
ThembaMtshelwane <thembamm3@gmail.com>
2024-11-17 12:56:58 +0200

    social media v-3 backend

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+import tweetRoutes from "./routes/tweet.route.js";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+const __dirname = path.resolve();
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE,PATCH",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use("/api/users", userRoutes);
+app.use("/api/tweets", tweetRoutes);
+
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started at http://localhost:${PORT}`);
+});
